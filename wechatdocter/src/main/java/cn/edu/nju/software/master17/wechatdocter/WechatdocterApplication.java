094af4b3b0d4f234797e232ed3fd90337d9ce084
Shengchao Chen <275838287@qq.com>
2017-12-04 22:24:32 +0800

    init

---
+++ wechatdocter/src/main/java/cn/edu/nju/software/master17/wechatdocter/WechatdocterApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.software.master17.wechatdocter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatdocterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatdocterApplication.class, args);
+	}
+}
---
+++ wechatdocter/src/test/java/cn/edu/nju/software/master17/wechatdocter/WechatdocterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.nju.software.master17.wechatdocter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatdocterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
